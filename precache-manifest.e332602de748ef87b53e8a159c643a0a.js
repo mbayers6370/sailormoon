5a1eb2d43715a918e695391d73c8e96e323a3a7a
Matthew Bayers <mbayers6370@users.noreply.github.com>
2020-04-25 17:30:50 -0700

    Updates

---
+++ precache-manifest.e332602de748ef87b53e8a159c643a0a.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "871dbdab605ebab2278177ccd0598748",
+    "url": "/sailormoon/index.html"
+  },
+  {
+    "revision": "0b88810eb2b61ecd881d",
+    "url": "/sailormoon/static/css/main.2f54170c.chunk.css"
+  },
+  {
+    "revision": "c85f02840c7df2bb9f18",
+    "url": "/sailormoon/static/js/2.964ea94d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sailormoon/static/js/2.964ea94d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b88810eb2b61ecd881d",
+    "url": "/sailormoon/static/js/main.634b4593.chunk.js"
+  },
+  {
+    "revision": "e5b8afbd1d6cb01e7229",
+    "url": "/sailormoon/static/js/runtime-main.f925a388.js"
+  },
+  {
+    "revision": "7f1d4d1d9df523c40e70e5e5693a4d44",
+    "url": "/sailormoon/static/media/01-Unfinished-Diagram.7f1d4d1d.png"
+  },
+  {
+    "revision": "5ff5f2a66527cf23f998b8c7c18b33d8",
+    "url": "/sailormoon/static/media/02-Finished-Diagram.5ff5f2a6.png"
+  },
+  {
+    "revision": "e4310309431215b4f7052a973ea90245",
+    "url": "/sailormoon/static/media/background.e4310309.jpg"
+  },
+  {
+    "revision": "aad8e3bf93e0bea163d04f30153e0ea2",
+    "url": "/sailormoon/static/media/backgroundDiv.aad8e3bf.jpg"
+  },
+  {
+    "revision": "a741657cc0b804f9d6ff90f67f536a49",
+    "url": "/sailormoon/static/media/sailorJupiter.a741657c.png"
+  },
+  {
+    "revision": "d8c9da3be6c61a8edce3d39f2c628416",
+    "url": "/sailormoon/static/media/sailorMars.d8c9da3b.png"
+  },
+  {
+    "revision": "190834ab2b16aa30802772e8a24174c8",
+    "url": "/sailormoon/static/media/sailorMercury.190834ab.png"
+  },
+  {
+    "revision": "e729864acd51c12b0a18d4923903f00f",
+    "url": "/sailormoon/static/media/sailorMoon.e729864a.png"
+  },
+  {
+    "revision": "b97c058578db1a80fd81731c1ca39ff8",
+    "url": "/sailormoon/static/media/sailorMoonTitle.b97c0585.png"
+  },
+  {
+    "revision": "7da4c19d08db6b970e2bdedbc9d33293",
+    "url": "/sailormoon/static/media/sailorNeptune.7da4c19d.png"
+  },
+  {
+    "revision": "738497b6b3e21b6ad00c04f401cf7d27",
+    "url": "/sailormoon/static/media/sailorPluto.738497b6.png"
+  },
+  {
+    "revision": "152f31c275535687117a2b346ca8dc3a",
+    "url": "/sailormoon/static/media/sailorSaturn.152f31c2.png"
+  },
+  {
+    "revision": "970fe852e4038cbd2b9940758edf8b83",
+    "url": "/sailormoon/static/media/sailorUranus.970fe852.png"
+  },
+  {
+    "revision": "f36d03bbbec8f5cddef8f8462a6961fd",
+    "url": "/sailormoon/static/media/sailorVenus.f36d03bb.png"
+  },
+  {
+    "revision": "d1424e54a99fdcfa2b8c613cbc0d5492",
+    "url": "/sailormoon/static/media/tuxedoMask.d1424e54.png"
+  }
+]);
